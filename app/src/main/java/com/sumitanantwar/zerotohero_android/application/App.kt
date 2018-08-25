7d340fa40c4ab4fd36f4bb993bc544d72b5439f2
Sumit Anantwar <sumit.anantwar@gmail.com>
2018-08-26 00:17:02 +0300

    create the Application class This class is very important for initializing Global configs.

---
+++ app/src/main/java/com/sumitanantwar/zerotohero_android/application/App.kt
@@ -0,0 +1,13 @@
+package com.sumitanantwar.zerotohero_android.application
+
+import android.app.Application
+
+class App : Application() {
+
+    // ======= Application Lifecycle =======
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
